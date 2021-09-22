41f985dd6890bb0dab5c196d8fc6c294a657d678
Donna Lorenzo <dlorenzo24@hotmail.com>
2021-09-22 11:43:17 -0500

    Added more page functionality

--- Landing Page/js/app.js
+++ Landing Page/js/app.js
@@ -1,41 +1,40 @@
+/**
+ *
+ * Landing Page exercise.
+ * Code builds navegation dynamically,
+ * scrolls to sections from navegation,
+ * and highlights navegation link in navegation bar upon scrolling.
+ *
+ * Dependencies: None
+ *
+ * JS Version: ES2015/ES6
+ *
+ * JS Standard: ESlint
+ *
+*/
+
+
+// Get elements from the DOM
 const sections = document.querySelectorAll('section');
 const mainNav = document.querySelector('.main-nav');
 const openMenu = document.querySelector('.open-menu');
 const cancelMenu = document.querySelector('.closed-menu');
+const btnContainer = document.querySelector('.top-btn');
+const mainContainer = document.querySelector('main');
+const heroSection =document.querySelector('.hero-section');
 
+
+
+// Event listens from document to load and runs makeActive function
 document.addEventListener('DOMContentLoad', makeActive);
 
-// Grab and make active navlink
-function makeActive(navEl){
-    const navLinks = document.querySelectorAll(`.nav-links li`);
-    let activeLink;
-    for(link of navLinks){
-        activeLink = link.getAttribute('data-nav');
-        if(navEl === activeLink){
-            link.classList.add('active');
-        } else{
-            link.classList.remove('active');
-        }
-    }
-}
 
 
-// Open and Closing menu functionality
-openMenu.addEventListener('click', () => {
-    openMenu.classList.add('hide');
-    cancelMenu.classList.add('show');
-    mainNav.classList.add('active');
-});
-
-cancelMenu.addEventListener('click', () => {
-    openMenu.classList.remove('hide');
-    cancelMenu.classList.remove('show');
-    mainNav.classList.remove('active');
-});
-
-
-// Creates a fragment of the navegation links and appends
-// them to the DOM
+/*
+	* @description: create nav-links dynamically and appends them to DOM
+	* @param {} 
+	* @return {} 
+*/
 function createNav() {
     const fragment = document.createDocumentFragment();
     const navUl = document.createElement('ul');
@@ -61,27 +60,69 @@ function createNav() {
 
     mainNav.appendChild(fragment);
 }
+
+// Call function
 createNav();
 
 
+
+/*
+	* @description: checks whether the passed section ID matches menu item data-nav attribute
+	* @param {document element} visible section upon scrolling html page
+	* @return {} 
+*/
+function makeActive(navEl){
+
+    // get all nav-links 
+    const navLinks = document.querySelectorAll(`.nav-links li`);
+    let activeLink;
+
+    for(link of navLinks){
+
+        // get nav-link attribute and check if it matches function param(NavEl)
+        activeLink = link.getAttribute('data-nav');
+        if(navEl === activeLink){
+            link.classList.add('active');
+        } else{
+            link.classList.remove('active');
+        }
+    }
+}
+
+
+
+// Listens form scroll event and calls grabActiveSection function
 document.addEventListener('scroll', grabActiveSection);
+
+
+/*
+	* @description: checks sections visibility upon scrolling & grabs current section
+	* @param {} 
+	* @return {} 
+*/
 function grabActiveSection() {
     let currentSection;
     for (section of sections){
-        // const box = section.getBoundingClientRect();
+
+        // stores section top value
         const sectionTop = section.offsetTop;
+
+        // stores section third part of height
         const sectionHeight= section.clientHeight/3;
-        // const viewportHeight = window.innerHeight || document.documentElement.clientHeight;
-        
+
         if(pageYOffset >= sectionTop - sectionHeight){
             currentSection = section.id;
             makeActive(currentSection);
         }
-        console.log(pageYOffset, sectionTop, sectionHeight);
     }
 }
 
-scrollToSection();
+
+/*
+	* @description: scrolls nav-links to view respective section
+	* @param {} 
+	* @return {} 
+*/
 function scrollToSection(){
     const navLinks = document.querySelectorAll(`.nav-links li`);
     navLinks.forEach(link => {
@@ -92,5 +133,81 @@ function scrollToSection(){
         });
     });
 }
+
+// Call function
+scrollToSection();
+
+
+/*
+	* @description: creates button to scroll to top
+	* @param {} 
+	* @return {} 
+*/
+function createtoTopBtn(){
+    const button = document.createElement('button');
+    btnContainer.appendChild(button);
+    button.id="scrollToTop";
+    button.innerHTML = '<i class="fas fa-chevron-up"></i>';
+}
+
+// Call function
+createtoTopBtn();
+
+
+// Events listens for scroll event and calls showToTopBtn function 
+document.addEventListener('scroll', showToTopBtn);
+
+
+/*
+	* @description: shows and hides to-top-btn based on document scroll value
+	* @param {} 
+	* @return {} 
+*/
+function showToTopBtn(){
+    if(pageYOffset >= 800){
+        btnContainer.classList.add('show');
+    } else{
+        btnContainer.classList.remove('show');
+    }
+}
+
+
+// Add scroll to top behavior to to-top-btn
+btnContainer.addEventListener('click', () =>{
+    heroSection.scrollIntoView({behavior: "smooth"});
+});
+
+
+// Open and Closing mobile menu functionality 
+openMenu.addEventListener('click', () => {
+    openMenu.classList.add('hide');
+    cancelMenu.classList.add('show');
+    mainNav.classList.add('active');
+});
+
+cancelMenu.addEventListener('click', () => {
+    openMenu.classList.remove('hide');
+    cancelMenu.classList.remove('show');
+    mainNav.classList.remove('active');
+});
+
+
+
+window.addEventListener('resize', () => {
+    const header = document.querySelector('header');
+    const height = header.offsetHeight;
+
+    console.log(height);
+    mainContainer.style.paddingTop = height + 'px';
+});
+
+window.addEventListener('load', () => {
+    const header = document.querySelector('header');
+    const height = header.offsetHeight;
+
+    console.log(height);
+    mainContainer.style.paddingTop = height + 'px';
+});
+
 
 
